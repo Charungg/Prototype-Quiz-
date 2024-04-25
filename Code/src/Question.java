6b716116435361a3993721de875511633b6fed09
Charungg <108029843+Charungg@users.noreply.github.com>
2024-04-25 16:03:40 +0100

    Add files via upload

--- Code/src/Question.java
+++ Code/src/Question.java
@@ -18,21 +18,28 @@ public class Question {
         questionsIdentifiers = new HashMap<>();
     }
 
-    public final void createQuestion(){
-        String uniqueIdentifier;
-        boolean createMoreQuestion = false;
-        Scanner console = new Scanner(System.in);
 
-        uniqueIdentifier = setQuestionIdentifier();
-        System.out.println(questionsIdentifiers);
-
-        //START FROM HERE
-
-
+    public void displayQuestion(){
+        for (String questionObject: questionsIdentifiers.keySet()){
+            System.out.println(questionObject);
+        }
     }
 
 
-    // Needed
+    public final void createQuestion(){
+        String uniqueIdentifier;
+        String moreQuestionAnswer;
+        boolean createMoreQuestion = true;
+        Scanner console = new Scanner(System.in);
+
+        uniqueIdentifier = setQuestionIdentifier();
+        uniqueIdentifierExist(uniqueIdentifier);
+
+        setQuestionType(uniqueIdentifier);
+        questionsIdentifiers.get(uniqueIdentifier).getLast().displayQuestion();
+    }
+
+
     public final String setQuestionIdentifier(){
         String uniqueIdentifier;
         String userInputModuleIdentifier;
@@ -61,10 +68,66 @@ public class Question {
         return uniqueIdentifier;
     }
 
-    public final void addQuestion(){
+
+    public void uniqueIdentifierExist(String uniqueIdentifier){
+        if (questionsIdentifiers.get(uniqueIdentifier) == null){
+            questionsIdentifiers.put(uniqueIdentifier,new ArrayList<>());
+            System.out.println("New Question Identifier");
+        }
+    }
+
+
+    public void setQuestionText(){
         Scanner console = new Scanner(System.in);
 
+        System.out.println("Enter A Question Text: ");
+        questionText = console.next();
+    }
 
+
+    public final void setQuestionType(String uniqueIdentifier){
+        int questionType;
+        boolean validQuestionType;
+
+        validQuestionType = false;
+        do{
+            questionType = setQuestionType();
+            switch(questionType){
+                case (1):
+                    questionsIdentifiers.get(uniqueIdentifier).add(new SingleChoiceQuestion(bankList));
+                    validQuestionType = true;
+                    break;
+                case (2):
+                    questionsIdentifiers.get(uniqueIdentifier).add(new SingleChoiceQuestion(bankList)); // FTB EDITION LATER
+                    validQuestionType = true;
+                    break;
+                default:
+                    System.out.println("Invalid Input, Enter A Integer From 1 - 2");
+            }
+        }while(!validQuestionType);
+    }
+
+
+    public final int setQuestionType(){
+        int questionType = -1;
+        Scanner console = new Scanner(System.in);
+
+        try{
+            System.out.println("""
+                                --------------------------
+                                1) Single Choice Question\s
+                                2) Fill The Blanks\s
+                                
+                                Enter A Question Type (1-2)\s
+                                --------------------------
+                                """);
+            questionType = console.nextInt();
+        }
+        catch (Exception e){
+            console.nextLine();
+            System.out.println("Invalid Input, Try Again");
+        }
+        return questionType;
     }
 }
 
--- Code/src/SingleChoiceQuestion.java
+++ Code/src/SingleChoiceQuestion.java
@@ -2,18 +2,96 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class SingleChoiceQuestion extends Question{
+    private int amountOfAnswerChoice;
 
     private ArrayList<String> answerChoice;
 
     private static final int amountOfQuestionChoice = 10;
 
-    private ArrayList<Integer> correctChoice;
+    private int correctChoice;
 
 
     public SingleChoiceQuestion(Bank setBankList) {
         super(setBankList);
         answerChoice = new ArrayList<>(amountOfQuestionChoice);
-        correctChoice = new ArrayList<Integer>();
+        createSingleChoiceQuestion();
+    }
+
+
+    public void displayQuestion(){
+        System.out.println(questionText);
+        for (String answer: answerChoice){
+            System.out.println(answer);
+        }
+        System.out.println(correctChoice);
+    }
+
+
+    public void createSingleChoiceQuestion(){
+        setQuestionText();
+        amountOfAnswerChoice = setAmountOfAnswerChoice();
+        setAnswerChoice(amountOfAnswerChoice);
+        setCorrectAnswer();
+
+    }
+
+
+    public int setAmountOfAnswerChoice(){
+        int amountOfAnswerChoice = 0;
+        boolean amountValid = false;
+        Scanner console = new Scanner(System.in);
+
+        do{
+
+            System.out.println("Enter Amount Of Answer Choice (1-10): ");
+            try{
+                amountOfAnswerChoice = console.nextInt();
+                if (amountOfAnswerChoice >=1 && amountOfAnswerChoice <= 10){
+                    amountValid = true;
+                }
+
+            }
+
+            catch(Exception e){
+                console.nextLine();
+                System.out.println("Please Enter A Integer");
+            }
+
+
+        }while(!amountValid);
+
+        return amountOfAnswerChoice;
+    }
+
+
+    public void setAnswerChoice(int amountOfAnswerChoice){
+        String answerChoiceText;
+        Scanner console = new Scanner(System.in);
+        console.useDelimiter("\\n");
+
+        for (int i=0; i<amountOfAnswerChoice; i++){
+            System.out.println("Enter Answer Choice" + "1: ");
+            answerChoiceText = console.next();
+            answerChoice.add(answerChoiceText);
+
+        }
+    }
+
+
+    // Not made withe error checking in mind, wanted to test.
+    // So fix data types and range after.
+    public void setCorrectAnswer(){
+        int correctAnswerIndexPosition;
+        Scanner console = new Scanner(System.in);
+
+        for (String answer: answerChoice){
+            System.out.println(answer);
+        }
+
+        System.out.println("Enter The Correct Answer: ");
+        correctAnswerIndexPosition = console.nextInt();
+
+        correctChoice = correctAnswerIndexPosition;
     }
 
 
